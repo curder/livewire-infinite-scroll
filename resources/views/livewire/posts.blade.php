402ea19e6f43a47f170e509e7e51f9f0d15d7aad
curder <q.curder@gmail.com>
2024-05-28 11:14:11 +0800

    Add @alpinejs/intersect plugin

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -7,6 +7,10 @@
     @endforeach
 
     @if($this->hasMorePages())
+        <div x-intersect="$wire.loadMore"></div>
+    @endif
+
+    @if($this->hasMorePages())
         <div class="pt-6 flex justify-center">
             <button wire:click="loadMore"
                     class="w-56 text-center text-blue-50 hover:text-blue-100 py-1 rounded-md bg-blue-500 hover:bg-blue-600">
